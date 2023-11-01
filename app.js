73cc041d9ac17b8820f8a89f7d07a204296a616e
Glaubert01 <124006122+Glaubert01@users.noreply.github.com>
2023-11-01 12:39:04 -0300

    criando mensagem de acertou

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let numeroSecreto = gerarNumeroAletorio();
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -7,5 +9,13 @@ exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
+    let chute = document.querySelector('input').value
 
+    if(chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!')
+    }
+}
+
+function gerarNumeroAletorio(){
+    return parseInt(math.random() * 10 + 1);
 }
